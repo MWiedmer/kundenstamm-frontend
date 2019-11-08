3e91f1c470d9ba449b64b3847502672ecc1afdf6
tzdwim <markus.wiedmer@bs.ch>
2019-11-08 14:58:10 +0100

    material mit bootstrap ersetzt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,12 @@ import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {KundenDetailComponent} from './kunden-detail/kunden-detail.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatListModule} from '@angular/material/list';
-import {MatTableModule} from '@angular/material/table';
 import {HttpClientModule} from '@angular/common/http';
-import {MatCardModule} from '@angular/material/card';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, KundenDetailComponent],
   // tslint:disable-next-line:max-line-length
-  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule, MatSliderModule, MatFormFieldModule, MatInputModule, MatListModule, MatTableModule, HttpClientModule, MatCardModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
